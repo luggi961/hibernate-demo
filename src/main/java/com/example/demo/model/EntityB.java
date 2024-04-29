a42452b1b985a9dc1245d9d2a2992aed992acba3
lukasbauer <lukas.bauer@holidu.com>
2024-04-29 14:17:59 +0200

    tried with hypersistence utils

--- src/main/java/com/example/demo/model/EntityB.java
+++ src/main/java/com/example/demo/model/EntityB.java
@@ -1,11 +1,11 @@
 package com.example.demo.model;
 
+import io.hypersistence.utils.hibernate.type.json.JsonType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+import org.hibernate.annotations.Type;
 
 @Entity
 @NoArgsConstructor
@@ -13,7 +13,8 @@ import org.hibernate.type.SqlTypes;
 public class EntityB extends AbstractEntity {
 
     @Column(columnDefinition = "jsonb")
-    @JdbcTypeCode(SqlTypes.JSON)
+    //@JdbcTypeCode(SqlTypes.JSON)
+    @Type(JsonType.class)
     private DtoB entity_data;
 
 }
